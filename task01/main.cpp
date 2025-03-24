c384315f96da62f119be86c739fc3b5657ea5c9c
Nikita-mnd <nikita.morozov.mnd@gmail.com>
2025-03-24 18:40:50 +0300

    task02

--- task01/main.cpp
+++ task01/main.cpp
@@ -1,16 +1,33 @@
 #include <iostream>
 using namespace std;
 int factorial(int number);
+//void test(int count) {
+//	cout << (count ++) << endl;
+//	test(count);
+//}
 int main() {
-
-	/*cout << "input number: ";
+	/*int count = 0;
+	test(count);*/
+//	while (true) {
+//
+//		cout << (count+=100000) << endl;
+//	}
+	cout << "input number: ";
 	int number;
 	cin >> number;
-	int result = factorial(number);
-	cout <<number<< "!= " << result;*/
-
-lebal1: 
-	cout << "this is goto" << endl;
-	goto lebal1;
-	return 0;
+	int result;
+	if (number < 0) {
+		result =  -1;
+	}
+	if (number == 0 || number == 1) {
+		result = 
+			1;
+	}
+//	int result = factorial(number);
+//	cout <<number<< "!= " << result;
+//
+//lebal1: 
+//	cout << "this is goto" << endl;
+//	goto lebal1;
+//	return 0;
 }
